c2f895742649a4ef9764e6808d05114016c5d5b1
lijinglong <2357607191@qq.com>
2015-07-07 11:30:43 +0800

    Initial Commit

---
+++ hero/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hero
+//
+//  Created by lijinglong on 15/7/7.
+//  Copyright (c) 2015年 lijinglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
