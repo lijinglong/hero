f5e86aba6b642309fec9ce932aee9e34c158212b
lijinglong <2357607191@qq.com>
2015-07-07 17:19:07 +0800

    add tag

--- hero/ViewController.m
+++ hero/ViewController.m
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) UIView *view1;
 @property (nonatomic, strong) UIView *view2;
 @property (nonatomic, strong) UIView *view3;
+@property (nonatomic, strong) UIView *currentView;
 
 @property (nonatomic, strong) UIImageView *roleImg;
 @property (nonatomic, strong) UIView *powerView;
@@ -37,15 +38,19 @@
 
     self.view1 = [[UIView alloc]initWithFrame:CGRectMake(10, 568 - 100, arc4random() % 50 + 10, 100)];
     self.view1.backgroundColor = [UIColor redColor];
+    self.view1.tag = 1000;
     [self.view addSubview:self.view1];
     
     self.view2 = [[UIView alloc] initWithFrame:CGRectMake(self.view1.frame.size.width + 20 + arc4random() % 60, 568 - 100, arc4random() % 50 + 10, 100)];
     self.view2.backgroundColor = [UIColor greenColor];
+    self.view2.tag = 1001;
     [self.view addSubview:self.view2];
     
     self.view3 = [[UIView alloc] initWithFrame:CGRectMake(320, 468, arc4random() % 50 + 10, 100)];
     self.view3.backgroundColor = [UIColor grayColor];
+    self.view3.tag = 1002;
     [self.view addSubview:self.view3];
+    self.currentView.tag = 1004;
     
     self.powerView = [[UIView alloc] initWithFrame:CGRectMake(20, 300, 10, 2)];
     self.powerView.backgroundColor = [UIColor orangeColor];
@@ -107,7 +112,10 @@
                 self.view2.frame = CGRectMake(10, self.view2.frame.origin.y, self.view2.frame.size.width, self.view2.frame.size.height);
                 self.view3.frame = CGRectMake(arc4random() % 100 + 20 + self.view2.frame.origin.x, self.view3.frame.origin.y, self.view3.frame.size.width, self.view3.frame.size.height);
             } completion:^(BOOL finished) {
-                
+                self.currentView.tag = self.view1.tag;
+                self.view1.tag = self.view2.tag;
+                self.view2.tag = self.view3.tag;
+                self.view3.tag = self.currentView.tag;
             }];
             
         }];
